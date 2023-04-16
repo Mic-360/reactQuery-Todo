fd619471a8b45ffcaf10b7160f76f5b735c6a138
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-04-17 00:12:14 +0530

    Query: fetching from server with useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
 import './App.css';
 
 function App() {
+  const url = 'http://localhost:3000/api/languages';
+
+  const {
+    data: languages,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['languages'],
+    queryFn: async () => {
+      const res = await fetch(url);
+      return await res.json();
+    },
+    select: (data) => data.language,
+  });
 
   return (
     <div className='App'>
       <h1>To do App</h1>
       <strong>Programming languages</strong>
+
+      {languages.map((language, index) => {
+        return (
+          <p key={index}>
+            <strong>{index + 1}</strong>.&nbsp;{language}
+          </p>
+        );
+      })}
+      
     </div>
   );
 }
