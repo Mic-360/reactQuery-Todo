9bdbdb1ae1bfb47831e3825ca5825c6a3ce1b476
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2023-04-17 00:06:51 +0530

    Query: initial ToDo files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>To do App</h1>
+      <strong>Programming languages</strong>
+    </div>
+  );
+}
+
+export default App;
